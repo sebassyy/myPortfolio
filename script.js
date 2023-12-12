e9f54fdf37e9750af5d52bb75474aba0015d3aef
sebassyy <98722667+sebassyy@users.noreply.github.com>
2023-12-12 17:47:06 +0800

    change height

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function movestar1() {
-    document.getElementById('star1').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
+    document.getElementById('star1').style.top = JSON.stringify(Math.floor(Math.random() * 400))+"px";
     document.getElementById('star1').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
 function movestar2() {
-    document.getElementById('star2').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
+    document.getElementById('star2').style.top = JSON.stringify(Math.floor(Math.random() * 400))+"px";
     document.getElementById('star2').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
 function movestar3() {
-    document.getElementById('star3').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
+    document.getElementById('star3').style.top = JSON.stringify(Math.floor(Math.random() * 400))+"px";
     document.getElementById('star3').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
 function movestar4() {
-    document.getElementById('star4').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
+    document.getElementById('star4').style.top = JSON.stringify(Math.floor(Math.random() * 400))+"px";
     document.getElementById('star4').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
 function movestar5() {
-    document.getElementById('star5').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
+    document.getElementById('star5').style.top = JSON.stringify(Math.floor(Math.random() * 400))+"px";
     document.getElementById('star5').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
 
