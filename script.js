61042b4c3d2ec5913ce39c12f797e44b78a2477b
sebassyy <98722667+sebassyy@users.noreply.github.com>
2023-12-12 17:31:51 +0800

    more major changes

--- script.js
+++ script.js
@@ -18,3 +18,9 @@ function movestar5() {
     document.getElementById('star5').style.top = JSON.stringify(Math.floor(Math.random() * 500))+"px";
     document.getElementById('star5').style.left = JSON.stringify(Math.floor(Math.random() * 1200))+"px";
 }
+
+setInterval(movestar1, 10000);
+setInterval(movestar2, 10000);
+setInterval(movestar3, 10000);
+setInterval(movestar4, 10000);
+setInterval(movestar5, 10000);
